c4d5f4f75b1b156adcd997d8270e91a9d7645a12
Lgras92 <92830392+Lgras92@users.noreply.github.com>
2022-03-26 16:02:20 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProyectoPotencias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
