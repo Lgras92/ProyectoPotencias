2ab58e8994d863d66e6f9656a924305a6149a356
Lgras92 <92830392+Lgras92@users.noreply.github.com>
2022-03-26 16:30:47 -0300

    Primer intento de programa de ptencia, problema con los ciclos

---
+++ Matem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectoPotencias
+{
+    public class Matem
+    {
+        public Matem() { }
+
+        public double Potencia(double pArgumento, int pExponete )
+        {
+            int exponete = pExponete;
+            double resultado = 1; // elemento neutro para la multiplicacion
+
+            while (exponete != 0)
+            {
+                if (exponete > 0)
+                { 
+                    resultado = resultado * pArgumento;
+                    exponete = exponete - 1;
+                }
+                if (exponete < 0)
+                {
+                    resultado = resultado / pArgumento;
+                    exponete = exponete + 1;
+                }
+            }
+            return resultado;
+        }
+    }
+}
