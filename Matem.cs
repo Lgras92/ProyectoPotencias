f974b5afd16f5db66017ceb9b2be86ad09ff86c8
Lgras92 <92830392+Lgras92@users.noreply.github.com>
2022-03-26 17:40:21 -0300

    Cambios para mejorar la complejidad de buvles y accesos a memoria

--- Matem.cs
+++ Matem.cs
@@ -10,7 +10,7 @@ namespace ProyectoPotencias
 
         public double Potencia(double pArgumento, int pExponete )
         {
-            int exponete = pExponete;
+            /*int exponete = pExponete;
             double resultado = 1; // elemento neutro para la multiplicacion
 
             while (exponete != 0)
@@ -26,7 +26,30 @@ namespace ProyectoPotencias
                     exponete = exponete + 1;
                 }
             }
-            return resultado;
+            return resultado;*/
+
+            try 
+            {
+                if (pArgumento == 0 && pExponete != 0)
+                { return 0; }
+                if (pArgumento >0)
+                {
+                    switch (pExponete)
+                    {
+                        case 0: return 1;
+
+                        case 1: return pArgumento;
+
+                        default: return pArgumento * Potencia(pArgumento, pExponete - 1);
+                    }
+                }
+
+                return -1;
+                
+            } 
+            catch { Console.WriteLine("error, ptencia indeterminada");
+                return -1;
+            }
         }
     }
 }
